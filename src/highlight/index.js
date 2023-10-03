ee9c9ca2b5875f5ef9c035b37c0ff5b7701e76c3
HungNguyen <baohung2412@gmail.com>
2023-10-03 14:29:17 +0700

    Start working on div comment

---
+++ src/highlight/index.js
@@ -0,0 +1,11 @@
+// template - highlight element
+// template - highlight toolbar
+// template - highlight menu
+
+// store - query string node
+// store - start / end offset
+// store - content highlight
+// store - uuid highlight
+
+// func - highlight by range, cast with highlight
+// func - remove highlight by id, remove cast
