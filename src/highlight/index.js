2dc5bdc3a13915c54335d03274a63977034277a6
HungNguyen <baohung2412@gmail.com>
2023-10-03 15:10:08 +0700

    Add remove highlight function

--- src/highlight/index.js
+++ src/highlight/index.js
@@ -9,3 +9,17 @@
 
 // func - highlight by range, cast with highlight
 // func - remove highlight by id, remove cast
+
+export const removeHighlight = (id) => {
+  // select element to unwrap
+  var el = document.getElementById(id);
+
+  // get the element's parent node
+  var parent = el.parentNode;
+
+  // move all children out of the element
+  while (el.firstChild) parent.insertBefore(el.firstChild, el);
+
+  // remove the empty element
+  parent.removeChild(el);
+};
